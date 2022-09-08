371db93455cd4ef3e244c9d8b83831658bd5656f
Andrey Nozdrenko <developer@devit.group>
2022-09-08 11:00:17 +0300

    added calendar to timeline

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -1,20 +1,49 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { UseContext } from "../context";
+import moment from "moment";
 
 
 const TimeLine = (props) => {
-    const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let days = [];
+
+    const rangeDates = () => {
+        let range = [];
+        const weekStart = moment().startOf('month').startOf('isoWeek');
+        const weekEnd = moment().endOf('month').endOf('isoWeek');
+
+        let diff = weekEnd.diff(weekStart, 'days')
+
+        for (let i = 0; i < diff; i++) {
+            range.push(moment(weekStart).add(i, 'days'))
+        }
+
+        return range;
+    }
+
+    days = rangeDates();
+    console.log(days);
 
     return (
         <div className={'time'}>
             {
-                <div className={'week'}>
-                    {
-                        week.map((item) => {
-                            return <div key={item}>{item}</div>
-                        })
-                    }
-                </div>
+                <React.Fragment>
+                    <div className={'week'}>
+                        {
+                            week.map((item) => {
+                                return <div key={item}>{item}</div>
+                            })
+                        }
+                    </div>
+
+                    <div className={'days'}>
+                        {
+                            days.map((day) => {
+                                return <div className={day.isSame(new Date(), "day") ?  'current' : 'day'} key={day.date()}>{day.date()}</div>
+                            })
+                        }
+                    </div>
+                </React.Fragment>
             }
         </div>
     );
